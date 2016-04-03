e1d40ad44909ebeac7ba0dfa49e1134600b8f2d9
王伟华 <webskywang@163.com>
2016-04-03 17:31:12 +0800

    2

--- KingContent/Schema/Schema.cs
+++ KingContent/Schema/Schema.cs
@@ -10,6 +10,35 @@ namespace KingContent
     public interface ISchemaBuilderCommand
     {
     }
-  
 
+    public abstract class SchemaCommand : ISchemaBuilderCommand
+    {
+        protected SchemaCommand(string name, SchemaCommandType type)
+        {
+            TableCommands = new List<TableCommand>();
+            Type = type;
+            WithName(name);
+        }
+
+        public string Name { get; private set; }
+        public List<TableCommand> TableCommands { get; private set; }
+
+        public SchemaCommandType Type { get; private set; }
+
+        public SchemaCommand WithName(string name)
+        {
+            Name = name;
+            return this;
+        }
+    }
+
+    public enum SchemaCommandType
+    {
+        CreateTable,
+        DropTable,
+        AlterTable,
+        SqlStatement,
+        CreateForeignKey,
+        DropForeignKey
+    }
 }
--- KingContent/Schema/Table.cs
+++ KingContent/Schema/Table.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+using System.Data;
 
 namespace KingContent
 {
@@ -9,8 +10,168 @@ namespace KingContent
         public TableCommand(string tableName)
         {
             TableName = tableName;
+        }  
+    }
+    public class AlterTableCommand : SchemaCommand
+    {
+        public AlterTableCommand(string name)
+            : base(name, SchemaCommandType.AlterTable)
+        {
         }
 
+        public void AddColumn(string columnName, DbType dbType, Action<AddColumnCommand> column = null)
+        {
+            var command = new AddColumnCommand(Name, columnName);
+            command.WithType(dbType);
 
+            if (column != null)
+            {
+                column(command);
+            }
+
+            TableCommands.Add(command);
+        }
+
+        public void AddColumn<T>(string columnName, Action<AddColumnCommand> column = null)
+        {
+            var dbType = SchemaUtils.ToDbType(typeof(T));
+            AddColumn(columnName, dbType, column);
+        }
+
+        public void DropColumn(string columnName)
+        {
+            var command = new DropColumnCommand(Name, columnName);
+            TableCommands.Add(command);
+        }
+
+        public void AlterColumn(string columnName, Action<AlterColumnCommand> column = null)
+        {
+            var command = new AlterColumnCommand(Name, columnName);
+
+            if (column != null)
+            {
+                column(command);
+            }
+
+            TableCommands.Add(command);
+        }
+
+        public void CreateIndex(string indexName, params string[] columnNames)
+        {
+            var command = new AddIndexCommand(Name, indexName, columnNames);
+            TableCommands.Add(command);
+        }
+
+        public void DropIndex(string indexName)
+        {
+            var command = new DropIndexCommand(Name, indexName);
+            TableCommands.Add(command);
+        }
+    }
+
+    public class CreateTableCommand : SchemaCommand
+    {
+        public CreateTableCommand(string name)
+            : base(name, SchemaCommandType.CreateTable)
+        {
+        }
+
+        public CreateTableCommand Column(string columnName, DbType dbType, Action<CreateColumnCommand> column = null)
+        {
+            var command = new CreateColumnCommand(Name, columnName);
+            command.WithType(dbType);
+
+            if (column != null)
+            {
+                column(command);
+            }
+            TableCommands.Add(command);
+            return this;
+        }
+
+        public CreateTableCommand Column<T>(string columnName, Action<CreateColumnCommand> column = null)
+        {
+            var dbType = SchemaUtils.ToDbType(typeof(T));
+            return Column(columnName, dbType, column);
+        }
+
+        /// <summary>
+        /// Defines a primary column as for content parts
+        /// </summary>
+        public CreateTableCommand ContentPartRecord()
+        {
+            Column<int>("Id", column => column.PrimaryKey().NotNull());
+
+            return this;
+        }
+
+        /// <summary>
+        /// Defines a primary column as for versionnable content parts
+        /// </summary>
+        public CreateTableCommand ContentPartVersionRecord()
+        {
+            Column<int>("Id", column => column.PrimaryKey().NotNull());
+            Column<int>("ContentItemRecord_id");
+            return this;
+        } 
+    }
+    public class DropTableCommand : SchemaCommand
+    {
+        public DropTableCommand(string name)
+            : base(name, SchemaCommandType.DropTable)
+        {
+        }
+    }
+    public class AddIndexCommand : TableCommand
+    {
+        public string IndexName { get; set; }
+
+        public AddIndexCommand(string tableName, string indexName, params string[] columnNames)
+            : base(tableName)
+        {
+            ColumnNames = columnNames;
+            IndexName = indexName;
+        }
+
+        public string[] ColumnNames { get; private set; }
+    }
+    public class DropIndexCommand : TableCommand
+    {
+        public string IndexName { get; set; }
+
+        public DropIndexCommand(string tableName, string indexName)
+            : base(tableName)
+        {
+            IndexName = indexName;
+        }
+    }
+    public class CreateForeignKeyCommand : SchemaCommand
+    {
+        public string[] DestColumns { get; private set; }
+
+        public string DestTable { get; private set; }
+
+        public string[] SrcColumns { get; private set; }
+
+        public string SrcTable { get; private set; }
+
+        public CreateForeignKeyCommand(string name, string srcTable, string[] srcColumns, string destTable, string[] destColumns) : base(name, SchemaCommandType.CreateForeignKey)
+        {
+            SrcColumns = srcColumns;
+            DestTable = destTable;
+            DestColumns = destColumns;
+            SrcTable = srcTable;
+        }
+    }
+
+    public class DropForeignKeyCommand : SchemaCommand
+    {
+        public string SrcTable { get; private set; }
+
+        public DropForeignKeyCommand(string srcTable, string name)
+            : base(name, SchemaCommandType.DropForeignKey)
+        {
+            SrcTable = srcTable;
+        }
     }
 }
