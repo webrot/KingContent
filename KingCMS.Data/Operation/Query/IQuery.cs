b655626a405ddbc3d8af4030883656a282561000
王伟华 <webskywang@163.com>
2017-02-07 14:16:33 +0800

    添加接口

---
+++ KingCMS.Data/Operation/Query/IQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KingCMS.Data.Operation.Query
+{
+    interface IQuery
+    {
+    }
+}
